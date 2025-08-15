0646ed2c76f4eb56f4b7a287ad6d206d5f6c2bac
sandovaljoaquin <joaquinsandoval74@yahoo.com>
2025-08-15 12:04:15 -0700

    Commit

---
+++ src/sustainable_harvest.R
@@ -0,0 +1,19 @@
+#' Maximum Sustainable Fishery Harvest
+#' 
+#' A little function to return the maximum sustainable harvest given the carrying capacity and growth rate. 
+#'
+#' @param K A number indicating the carrying capacity of the fishery.
+#' @param r A number indicating the intrinsic growth rate of the fishery. 
+#'
+#' @returns A number indicating the maximum annual sustainable harvest for the fishery. 
+#' @export
+#'
+#' @examples 
+# max_sustainable_harvest(K = 3.8e5, r = 0.62)
+
+max_sustainable_harvest <- function(K,r) {
+  harvest <- (K * r) / 4
+  return(harvest)
+}
+
+max_sustainable_harvest(K= 36000, r = 0.31)
